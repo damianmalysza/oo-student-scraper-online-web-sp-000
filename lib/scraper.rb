3d40510b4bfb1d8cd1bdd49523bca4c116a03096
Damian Malysza <damian.malysza@gmail.com>
2020-11-17 23:30:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require 'nokogiri'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    doc = Nokogiri::HTML(open(https://learn-co-curriculum.github.io/student-scraper-test-page/index.html))
+    doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"))
   end
 
   def self.scrape_profile_page(profile_url)
