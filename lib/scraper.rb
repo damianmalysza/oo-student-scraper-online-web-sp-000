5513a3c908918fdd22bde483f71a62b82769a819
Damian Malysza <damian.malysza@gmail.com>
2020-11-17 23:45:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,7 @@ require 'nokogiri'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    #loading the HTML element using Nokogiri and storing in a 'doc' variable
     doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"))
     #iterate over each student card
     doc.css(".student-card").collect do |student|
