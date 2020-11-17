53cf3d87a6712e2bbf9a70c63846fb0627d84304
Damian Malysza <damian.malysza@gmail.com>
2020-11-17 23:42:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,9 +7,9 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"))
     return_array = []
-    binding.pry
     student_profile = doc.css(".student-card")
     student_profile.collect do {|student|}
+      binding.pry
       return_array << {name: => student.css(".student-name").text, location: studnet.css(".student-location").text, profile_url: student.css("a").attr("href").text}
     end
     # name = student_profile.css(".student-name").text
