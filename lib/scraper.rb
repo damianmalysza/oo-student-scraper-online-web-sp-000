7a3f29085663e85d299e176adcb4c4f9dc02b556
Damian Malysza <damian.malysza@gmail.com>
2020-11-18 03:39:20 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,6 +20,7 @@ class Scraper
       social_med = social.attr("href").gsub("www.","")[/(?<=\/{2})(.*?)(?=\.)/]
       if social_med_arr.include?(social_med)
         return_hash[social_med.to_sym] = social.attr("href")
+      else
     end
     return_hash[:profile_quote] = doc.css(".vitals-text-container").css(".profile-quote").text
     return_hash[:bio] = doc.css(".description-holder").css("p").text
