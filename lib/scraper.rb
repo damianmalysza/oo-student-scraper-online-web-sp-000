a1c27065ca500da01c72cd6fb7f6bd70647a95e0
Damian Malysza <damian.malysza@gmail.com>
2020-11-18 03:45:02 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,12 +12,18 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
+    #loading the HTML elements of the profile page using Nokogiri
     doc = Nokogiri::HTML(open(profile_url))
+    #establish the expected social media sites to be linked
     social_med_arr = ["twitter","linkedin","github"]
+    #setting an empty hash to have elements loaded into
     return_hash = {}
+    #using Nokogiri css selector to parse for any potential social media elements
     social_media = doc.css(".social-icon-container").css("a")
+    #looping through the array, using regex to parse the type of social media based on url, using gsub to get rid of 'www.' as the regex captures strings in between '//' and '.'
     social_media.each do |social|
       social_med = social.attr("href").gsub("www.","")[/(?<=\/{2})(.*?)(?=\.)/]
+      #the regex above fails for blog posts because the site is named after a student, using an if/else expression to see if the parsed social media is part of the social_med_arr
       if social_med_arr.include?(social_med)
         return_hash[social_med.to_sym] = social.attr("href")
       else
@@ -26,7 +32,7 @@ class Scraper
     end
     return_hash[:profile_quote] = doc.css(".vitals-text-container").css(".profile-quote").text
     return_hash[:bio] = doc.css(".description-holder").css("p").text
-  
+
     return_hash
   end
 
